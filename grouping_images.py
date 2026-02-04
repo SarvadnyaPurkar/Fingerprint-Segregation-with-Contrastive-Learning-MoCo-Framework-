61c324d51d61f3a736bba730b0e159028341bc0f
SarvadnyaPurkar <153994776+SarvadnyaPurkar@users.noreply.github.com>
2026-02-04 21:42:04 +0530

    Update grouping_images.py

--- grouping_images.py
+++ grouping_images.py
@@ -2,7 +2,8 @@ import os
 import shutil
 
 # Define the root folder containing the images
-root_folder = r"C:\\Users\\sarva\\Downloads\\FingerPrintDataset\\FingerPrintProject\\denoise_images"
+base_dir = "data" 
+root_folder = os.path.join(base_dir, "denoise_images")
 
 # Get a list of all images in the folder and sort them to ensure the order is maintained
 images = sorted(os.listdir(root_folder))
@@ -25,4 +26,5 @@ for i in range(200):
         shutil.move(src_path, dest_path)
 
 print("Images organized into folders successfully!")
+
 
